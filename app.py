70e9ceded4cdab506fba030b2e20612ddbbfec0d
1208kelly <1208kelly@gmail.com>
2020-08-18 21:48:18 +0000

    Adding recipe page

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def recipe():
+def home():
     return 'Ven a comer'
 
+
+@app.route("/<name>")
+def recipe(name):
+    return f"Recipes for {name}"
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
